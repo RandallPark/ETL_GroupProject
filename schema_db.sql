f5b45d506dad16822bce418f6c283ffb4f7e5bde
Chris Wong <chriswong90012@yahoo.com>
2019-04-09 20:59:45 -0700

    Changes to sql file

--- schema_db.sql
+++ schema_db.sql
@@ -9,11 +9,13 @@ USE top50_db;
 
 
 CREATE TABLE country (
-	country_code varchar(50) NOT NULL,
+	country_code varchar(3) NOT NULL,
     country_name varchar(50) NOT NULL,
 	alpha_2 varchar(50) NOT NULL,
-    un_code INT NOT NULL
+    un_code INT NOT NULL,
 
+	PRIMARY KEY (country_code)
+    
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -28,7 +30,7 @@ CREATE TABLE song (
 
 CREATE TABLE playlist (
 	id INT NOT NULL,
-    country_code varchar(100) NOT NULL,
+    country_code varchar(3) NOT NULL,
     spotify_url varchar(100) NOT NULL,
     playlist_id varchar(100) NOT NULL,
     
